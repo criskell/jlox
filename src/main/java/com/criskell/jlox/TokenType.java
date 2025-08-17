c68c0d5b057dbd1eaaedec2116d1096853cddca1
criskell <cris.moraes.ds@gmail.com>
2025-08-16 21:19:40 -0300

    feat: lexer

---
+++ src/main/java/com/criskell/jlox/TokenType.java
@@ -0,0 +1,47 @@
+package com.criskell.jlox;
+
+enum TokenType {
+    // Tokens com apenas um caractere.
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+    // Tokens com um ou mais caracteres.
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+    // Literais.
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+    // Palavras-chave.
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+    EOF
+}
