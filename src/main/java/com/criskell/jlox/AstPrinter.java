dad569c3b913a6a23e24939602ce26436cee3769
criskell <cris.moraes.ds@gmail.com>
2025-12-01 04:41:49 -0300

    fix: add logical expr stub in ast printer

--- src/main/java/com/criskell/jlox/AstPrinter.java
+++ src/main/java/com/criskell/jlox/AstPrinter.java
@@ -1,6 +1,7 @@
 package com.criskell.jlox;
 
 import com.criskell.jlox.Expr.Assign;
+import com.criskell.jlox.Expr.Logical;
 import com.criskell.jlox.Expr.Ternary;
 import com.criskell.jlox.Expr.Variable;
 
@@ -42,6 +43,12 @@ public class AstPrinter implements Expr.Visitor<String> {
 
     @Override
     public String visitAssignExpr(Assign expr) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public String visitLogicalExpr(Logical expr) {
         // TODO
         return null;
     }
