3a45c404a2bc8e73e675bc0befb65984fbbeca89
criskell <cris.moraes.ds@gmail.com>
2025-12-01 06:51:46 -0300

    feat: add clock native function

--- src/main/java/com/criskell/jlox/Interpreter.java
+++ src/main/java/com/criskell/jlox/Interpreter.java
@@ -6,8 +6,26 @@ import java.util.List;
 import com.criskell.jlox.Expr.Ternary;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
-    private Environment environment = new Environment();
+    public Environment globals = new Environment();
+    private Environment environment = globals;
     private static class BreakException extends RuntimeException {}
+
+    Interpreter() {
+        globals.define("clock", new LoxCallable() {
+            @Override
+            public int arity() { return 0; }
+
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                return (double)System.currentTimeMillis() / 1000.0;
+            }
+
+            @Override
+            public String toString() {
+                return "<native fn>";
+            }
+        });
+    }
 
     public void interpret(List<Stmt> statements) {
         try {
