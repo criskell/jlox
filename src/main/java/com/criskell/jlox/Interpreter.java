d3fa00bcd300f974acae259887ac7826d35a084a
criskell <cris.moraes.ds@gmail.com>
2025-12-02 06:28:16 -0300

    feat: add property accessors

--- src/main/java/com/criskell/jlox/Interpreter.java
+++ src/main/java/com/criskell/jlox/Interpreter.java
@@ -59,6 +59,20 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
         return evaluate(expr.right);
     }
+
+    @Override
+    public Object visitSetExpr(Expr.Set expr) {
+        Object object = evaluate(expr.object);
+
+        if (!(object instanceof LoxInstance)) {
+            throw new RuntimeError(expr.name, "Only instances have fields.");
+        }
+
+        Object value = evaluate(expr.value);
+
+        ((LoxInstance)object).set(expr.name, value);
+        return value;
+    }
     
     @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
@@ -362,6 +376,17 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         }
 
         return function.call(this, arguments);
+    }
+
+    @Override
+    public Object visitGetExpr(Expr.Get expr) {
+        Object object = evaluate(expr.object);
+
+        if (object instanceof LoxInstance) {
+            return ((LoxInstance) object).get(expr.name);
+        }
+        
+        throw new RuntimeError(expr.name, "Only instances have properties.");
     }
 
     @Override
--- src/main/java/com/criskell/jlox/LoxInstance.java
+++ src/main/java/com/criskell/jlox/LoxInstance.java
@@ -1,10 +1,26 @@
 package com.criskell.jlox;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoxInstance {
     private LoxClass klass;
+    private final Map<String, Object> fields = new HashMap<>();
 
     public LoxInstance(LoxClass klass) {
         this.klass = klass;
+    }
+
+    public Object get(Token property) {
+        if (!fields.containsKey(property.lexeme)) {
+            throw new RuntimeError(property, "Undefined property '" + property.lexeme + "'");
+        }
+
+        return fields.get(property.lexeme);
+    }
+
+    public void set(Token name, Object value) {
+        fields.put(name.lexeme, value);
     }
 
     @Override
