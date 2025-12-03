d65803a9e8f2268b0a3006553570576cf9b9c79f
criskell <cris.moraes.ds@gmail.com>
2025-12-03 05:32:00 -0300

    feat: implement inheritance

--- src/main/java/com/criskell/jlox/Parser.java
+++ src/main/java/com/criskell/jlox/Parser.java
@@ -270,6 +270,14 @@ public class Parser {
 
     private Stmt classDeclaration() {
         Token name = consume(IDENTIFIER, "Expect class name.");
+
+        Expr.Variable superclass = null;
+
+        if (match(LESS)) {
+            consume(IDENTIFIER, "Expect superclass name.");
+            superclass = new Expr.Variable(previous());
+        }
+
         consume(LEFT_BRACE, "Expect '{' before class body.");
 
         List<Stmt.Function> methods = new ArrayList<>();
@@ -280,7 +288,7 @@ public class Parser {
 
         consume(RIGHT_BRACE, "Expect '}' after class body.");
 
-        return new Stmt.Class(name, methods);
+        return new Stmt.Class(name, superclass, methods);
     }
 
     private Expr comma() {
@@ -352,6 +360,11 @@ public class Parser {
     // Returns the most recently consumed token.
     private Token previous() {
         return tokens.get(current - 1);
+    }
+
+    private boolean checkPrevious(TokenType type) {
+        if (current <= 0) return false;
+        return previous().type == type;
     }
 
     private ParseError error(Token token, String message) {
@@ -537,13 +550,23 @@ public class Parser {
     }
 
     private Expr primary() {
-        if (match(FUN) || previous().type == TokenType.FUN) return anonymousFunction();
+        if (match(FUN) || checkPrevious(TokenType.FUN)) return anonymousFunction();
         if (match(FALSE)) return new Expr.Literal(false);
         if (match(TRUE)) return new Expr.Literal(true);
         if (match(NIL)) return new Expr.Literal(null);
 
         if (match(NUMBER, STRING)) {
             return new Expr.Literal(previous().literal);
+        }
+
+        if (match(SUPER)) {
+            Token keyword = previous();
+
+            consume(DOT, "Expect '.' after 'super'.");
+
+            Token method = consume(IDENTIFIER, "Expect superclass method name.");
+
+            return new Expr.Super(keyword, method);
         }
 
         if (match(THIS)) return new Expr.This(previous());
--- src/main/java/com/criskell/jlox/Resolver.java
+++ src/main/java/com/criskell/jlox/Resolver.java
@@ -28,7 +28,8 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     private enum ClassType {
         NONE,
-        CLASS
+        CLASS,
+        SUBCLASS
     }
 
     private ClassType currentClass = ClassType.NONE;
@@ -113,6 +114,19 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         declare(stmt.name);
         define(stmt.name);
 
+        if (stmt.superclass != null && stmt.name.lexeme.equals(stmt.superclass.name.lexeme)) {
+            Jlox.error(stmt.superclass.name, "A class can't inherit from itself.");
+        }
+
+        if (stmt.superclass != null) {
+            currentClass = ClassType.SUBCLASS;
+
+            resolve(stmt.superclass);
+
+            beginScope();
+            scopes.peek().put("super", true);
+        }
+
         beginScope();
         scopes.peek().put("this", true);
 
@@ -127,6 +141,8 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         }
 
         endScope();
+
+        if (stmt.superclass != null) endScope();
 
         currentClass = enclosingClass;
         return null;
@@ -257,6 +273,19 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     public Void visitSetExpr(Expr.Set expr) {
         resolve(expr.value);
         resolve(expr.object);
+        return null;
+    }
+
+    @Override
+    public Void visitSuperExpr(Expr.Super expr) {
+        if (currentClass == ClassType.NONE) {
+            Jlox.error(expr.keyword, "Can't use 'super' outside of a class.");
+        } else if (currentClass != ClassType.SUBCLASS) {
+            Jlox.error(expr.keyword, "Can't use 'super' in a class with no superclass.");
+        }
+
+        // We store the number of hops required for the interpreter to find the environment where the superclass was defined.
+        resolveLocal(expr, expr.keyword);
         return null;
     }
 
